49d2fd73a33f1f0fbd1607efbe9df6016cb23a5a
psanguinet <sanguinet@gmail.com>
2016-03-09 02:18:57 -0300

    Se valida nombre  de  usuario  (12  caracteres  máximo  y  validar  que  sea  un  texto  alfanumérico  sin espacios)

--- Codigo/Obligatorio/Modelo/Models/AccountModels.cs
+++ Codigo/Obligatorio/Modelo/Models/AccountModels.cs
@@ -48,8 +48,10 @@ namespace Modelo.Models
 
     public class LoginModel
     {
-        [Required(ErrorMessage = "El usuario es requerido.")]
+        [Required(ErrorMessage = "El nombre de usuario es requerido"), DataType(DataType.Text)]
         [Display(Name = "Usuario")]
+        [StringLength(12,ErrorMessage="No puede tener mas de 12 caracteres")]
+        [RegularExpression(@"^\S*$", ErrorMessage = "No se permiten espacios")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "La contraseña es requerida.")]
