9c3057597fd6129e0913b6dfbb367652f40e1855
psanguinet <sanguinet@gmail.com>
2016-03-10 19:22:32 -0300

    10) Un paciente puede consultar sus futuras consultas.

---
+++ Codigo/Obligatorio/BusinessLogic/Logic/ConsultaLogic.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Model;
+using Modelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BusinessLogic.Logic
+{
+   
+    public class ConsultaLogic : IConsultaLogic
+    {
+
+        public IEnumerable<Modelo.Models.Agenda> ListFuturasConsultasByPaciente(int PacienteID)
+        {
+            IEnumerable<Agenda> result = new List<Agenda>();
+            try
+            {
+                using (Context db = new Context())
+                {
+                    DateTime dateFrom = DateTime.Now;
+                    result = db.Agendas.Include("Paciente").Include("Doctor").Where(a => a.Paciente.PacienteID == PacienteID && a.Fecha >= dateFrom).OrderByDescending(a => a.Fecha).ToList();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return result;
+        }
+
+        public void Dispose()
+        {
+            GC.Collect();
+        }
+    }
+}
---
+++ Codigo/Obligatorio/BusinessLogic/Logic/IConsultaLogic.cs
@@ -0,0 +1,14 @@
+﻿using Modelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Logic
+{
+    public interface IConsultaLogic : IDisposable
+    {
+        IEnumerable<Agenda> ListFuturasConsultasByPaciente(int PacienteID);
+    }
+}
