f16c3c6bdfdf03933307b7d0f6c11196421120b6
psanguinet <sanguinet@gmail.com>
2016-03-12 21:17:18 -0300

    Se agrega resultados de analisis clinicos

---
+++ Codigo/Obligatorio/BusinessLogic/Logic/IResultadosAnalisisLogic.cs
@@ -0,0 +1,19 @@
+﻿using Modelo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Logic
+{
+    public interface IResultadosAnalisisLogic : IDisposable
+    {
+        IEnumerable<ResultadoAnalisis> ListadoResultadoAnalisis(Paciente paciente);
+        IEnumerable<ResultadoAnalisis> ListadoResultadoAnalisis(Doctor doctor);
+        ResultadoAnalisis GetResultadoAnalisis(int id);
+        void Save(ResultadoAnalisis resultadoAnalisis);
+        void Delete(int id);
+        void Edit(ResultadoAnalisis resultadoAnalisis);
+    }
+}
